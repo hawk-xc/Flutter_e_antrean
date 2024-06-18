858bb67658d6ced0a2157901b7c3328e47f2e6d2
wahyutc <hawkxc068@gmail.com>
2024-06-18 14:06:33 +0700

    add bottom navigation links

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_e_service_app/ui/login_view.dart';
 import 'package:flutter_e_service_app/ui/dashboard_view.dart';
+import 'package:flutter_e_service_app/ui/profile_view.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -9,33 +10,77 @@ void main() => runApp(
           primarySwatch: Colors.blue,
           fontFamily: 'Poppins',
         ),
-        home: const DashboardView(),
-        // home: LoginView(),
-        // home: RegisterView(),
-        // home: ProfileView(),
+        home: const MainPage(),
       ),
     );
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("hello world"),
-    );
-  }
+  State<MainPage> createState() => MainPageState();
 }
 
-class MainMenu extends StatelessWidget {
-  const MainMenu({super.key});
+class MainPageState extends State<MainPage> {
+  List<Widget> body = const [
+    DashboardView(),
+    Icon(Icons.laptop),
+    Icon(Icons.person),
+    ProfileView(),
+  ];
+
+  int _selectedIndex = 1;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('home base')),
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 90,
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.laptop_mac,
+                size: 30,
+              ),
+              label: 'Laptop',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.layers,
+                size: 30,
+              ),
+              label: 'Papers',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+                size: 30,
+              ),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black38,
+          // backgroundColor: Colors.black,
+          onTap: _onItemTapped,
+        ),
       ),
+      body: body[_selectedIndex],
     );
   }
 }
